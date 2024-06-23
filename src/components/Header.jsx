8e9e9bf26daa40685d01cb2d8d9e5bd76b0f0ac0
Rajganez <ganezsouth@live.com>
2024-06-24 00:43:52 +0530

    Front End for API created with MongoDB

---
+++ src/components/Header.jsx
@@ -0,0 +1,72 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <div className="container">
+        <nav className="navbar navbar-dark bg-dark fixed-top">
+          <div className="container-fluid">
+            <NavLink className="navbar-brand" to="/">
+              Hiran Tech and Teachings
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasDarkNavbar"
+              aria-controls="offcanvasDarkNavbar"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="offcanvas offcanvas-end text-bg-dark"
+              tabIndex="-1"
+              id="offcanvasDarkNavbar"
+              aria-labelledby="offcanvasDarkNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">
+                  Admin Login
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close btn-close-white"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="offcanvas-body">
+                <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/students"
+                      style={({ isActive }) => ({
+                        color: isActive ? "#B100B1" : "#6597E6",
+                      })}
+                    >
+                      Students
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/mentors"
+                      style={({ isActive }) => ({
+                        color: isActive ? "#B100B1" : "#6597E6",
+                      })}
+                    >
+                      Mentors
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Header;
